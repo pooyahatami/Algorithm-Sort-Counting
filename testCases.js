1b9af56dc5f19c10eb929394677134bb7121eae5
Pooya Hatami <am.hatami@yahoo.com>
2017-07-02 23:31:33 -0700

    Update testCases.js

--- testCases.js
+++ testCases.js
@@ -2,18 +2,24 @@
  * Counting sort algorithm !
  * Class	Sorting algorithm
  * Data structure	Array
- * Worst-case performance	О(n+k) comparisons, swaps
- * Best-case performance	O(n+k) comparisons, O(1) swaps
- * Average performance	О(n+k) comparisons, swaps
- * Worst-case space complexity
- *   where k is the the bigest number and n is the
+ * Worst-case performance	O(n+k)
+ * Best-case performance	O(n+k)
+ * Average performance	O(n+k)
+ * Worst-case space complexity 
+ *   where n is the number of elements in input array and k is the range of input.
  *   size of the input array.
+ *   Note: if k is greater than log(n) then an n*log(n) algorithm would be a
+ *         better fit. In reality we can always change the radix to make k
+ *         less than log(n).
  *
  * Author: Pooya Hatami
  */
 
-var rectcs = require('./node-sort-counting');
+var nodesort = require('./node-sort-counting');
+var displaymode = "No"; //"Yes";  // "Yes" for more details of algorithm progress 
+var base = 10;
 
+var arrin00 = [20, 8 , -11, 12, 22 , 9 , 10 ];
 var arrin01 = [20, 8 , 48, 120, 220 , 390 , 1000 ];
 var arrin02 = [20, 8 , 480 , 120, 220 , 390 , 1000 ];
 var arrin03 = [1120, 800 , 480 , 120, 20 , 390 , 1000 ];
@@ -21,22 +27,46 @@ var arrin04 = ['g', 'e', 'e', 'k', 's', 'f', 'o',
                       'r', 'g', 'e', 'e', 'k', 's'];
 var arrin05 = [1, 3, 7, 25, 12, 9, 8,
                       121, 221, 10, 18, 29, 49];
+var arrin06 = [1, 3, -7, 25, 12, 9, 8,
+                      121, 221, -10, 18, 29, 49];
+var arrin07 = [1, 3, 7000000000000000000, 25, 12, 9, 8,
+                      121, 221, 100000000000000000000000000 , 18, 290000000000000000000, 49];
+var arrin08 = [1, 3, 75432, 25, 12, 9, 8,
+                      121, 221, 976562 , 18, 299999, 49];
+var arrin09 = [0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434 , 0.611 , 0.621 ];
+var arrin10 = [1,342, 14,293 , 0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434 , 0.611 , 0.621 ];
+var arrin11 = [5, 8 , 11, 12, 2 , 9 , 10 , 4 , 11, 10, 12, 7, 9 ];
+var arrin12 = "";
+//var arrin13 = [A7,02,22,77,37,15,00,40,B00,75,04,05,07,75,52,12,50,77,71,D07];    //base16
+var arrin14 = [1001,101010,11,10,01,111,100,1000,11100,10110,101,100010,0111,101,11111,1000001,1,0,111,11010];   //base 2
+var arrin15 = [7,2,22,77,37,15,10770,740,70,75,04,5,107,75,52,12,50,177,71,207];   //base 8
 
 
-function solveIS(arr,range) {
-    var arr_original = arr.toString() ;
-    var result = rectcs.countingSort(arr,range);
-    if (result==-1){
-    console.log("Fail attempt to sort array ["+arr_original+" ] by Insertion Sort " );
-    } else {
-    console.log("Success attempt to sort array ["+arr_original+" ] and result is : [ "
+function solveSorting(inputArray) {
+    var arr_original = inputArray.toString() ;
+    var sortedArray = inputArray;
+
+    nodesort(inputArray, displaymode,  function(err,sortRef) {
+        if (err) {
+	         console.log(err);
+	                }
+	      else {
+           var result = sortRef.countingSort(inputArray);
+	         console.log("Success attempt to sort array \r\n \t ["+arr_original+" ] \r\n and result is : \r\n \t [ "
                 + result + " ]" );
-    }
-   
-   console.log("----------------------------------------------------------");     
-}
+  
+	      sortedArray = result;
+	            }
+	      console.log("----------------------------------------------------------"); 
+    });
+    
+    return sortedArray;
+};
 
-solveIS(arrin01,1000);
-solveIS(arrin05,256);
-solveIS(arrin03,1120);
-
+solveSorting(arrin01);
+solveSorting(arrin00);
+solveSorting(arrin03);
+solveSorting(arrin11);
+solveSorting(arrin12);
+solveSorting(arrin14);
+solveSorting(arrin15);
